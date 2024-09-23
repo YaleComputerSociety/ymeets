e67c16cefefc79efa68a2c4fd2981b6f12942844
Ethan M <ethan.mathieu@yale.edu>
2024-09-23 00:03:24 -0400

    lol keys were still in a comment

--- src/firebase/gapi_keys.ts
+++ src/firebase/gapi_keys.ts
@@ -1,2 +1,1 @@
-export const REACT_APP_API_KEY_GAPI = 'AIzaSyCOJpO0VhGWM91xv77cmWd7-CZno_4zeaE'
-export const REACT_APP_CLIENT_ID_GAPI = '83459975838-fqj9tuogpa17urv03jsaos2nbjfde9ne.apps.googleusercontent.com'
+export {}
