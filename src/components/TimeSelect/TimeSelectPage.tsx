469c7ed5e61c3e3c0f98474044fed2fb73f7ca4c
Jeet Parikh <jeetparikh06@gmail.com>
2025-03-06 16:58:07 -0500

    mobile compatibiltiy, cleaned up duplicate

--- src/components/TimeSelect/TimeSelectPage.tsx
+++ src/components/TimeSelect/TimeSelectPage.tsx
@@ -593,8 +593,72 @@ function TimeSelectPage() {
           <div className="w-full">
             <div className="flex flex-col space-y-0 mb-2">
               <div className="flex justify-center ml-2 mr-2 md:justify-start md:m-5 mb-1">
-                <div className="flex items-center w-full max-w-full justify-between space-x-5">
+                {/* Mobile layout */}
+                <div className="w-full flex flex-col md:hidden">
+                  <div className="w-full">
+                    <TimezoneChanger
+                      theCalendarFramework={[
+                        calendarFramework,
+                        setCalendarFramework,
+                      ]}
+                      initialTimezone={
+                        getTimezone()
+                          ? getTimezone()
+                          : Intl.DateTimeFormat().resolvedOptions().timeZone
+                      }
+                    />
+                  </div>
 
+                  <div className="flex justify-around mt-3 align-middle">
+                    {getAccountId() !== '' ? (
+                      <div className="z-60 mb-4">
+                        <ButtonSmall
+                          bgColor="primary"
+                          textColor="white"
+                          onClick={handleToggleGCalAvailabilitiesClick}
+                        >
+                          Show GCal Events
+                        </ButtonSmall>
+                      </div>
+                    ) : (
+                      <div className="mb-4">
+                        <button
+                          className="w-full font-bold rounded-full shadow-md bg-white text-gray-600 py-3 px-4 text-sm
+                flex items-center justify-center transform transition-transform hover:scale-95 active:scale-100"
+                          onClick={() => {
+                            signInWithGoogle(
+                              undefined,
+                              gapi,
+                              handleIsSignedIn
+                            ).then((loginSuccessful) => {
+                              if (loginSuccessful) {
+                                window.location.reload();
+                              } else {
+                                console.error('login failed');
+                              }
+                            });
+                          }}
+                        >
+                          <img src={LOGO} alt="Logo" className="mr-2 h-5" />
+                          Sign in to access GCal
+                        </button>
+                      </div>
+                    )}
+                    <div className="z-60 mb-4">
+                      <ButtonSmall
+                        bgColor="primary"
+                        textColor="white"
+                        themeGradient={false}
+                        onClick={handleSubmitAvailability}
+                      >
+                        Next <span className="ml-1">&#8594;</span>
+                      </ButtonSmall>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Desktop layout */}
+                <div className="hidden md:flex items-center w-full max-w-full justify-between space-x-5">
                   <div className="flex-grow">
                     <TimezoneChanger
                       theCalendarFramework={[
@@ -634,7 +698,7 @@ function TimeSelectPage() {
                     <div className="mb-4 lg:mb-0">
                       <button
                         className="w-full lg:w-auto font-bold rounded-full shadow-md bg-white text-gray-600 py-3 px-4 text-sm lg:text-base
-                            flex items-center justify-center transform transition-transform hover:scale-95 active:scale-100"
+              flex items-center justify-center transform transition-transform hover:scale-95 active:scale-100"
                         onClick={() => {
                           signInWithGoogle(
                             undefined,
@@ -654,7 +718,7 @@ function TimeSelectPage() {
                           alt="Logo"
                           className="mr-2 h-5 lg:h-6"
                         />
-                        Sign in with Google to access GCal
+                        Sign in to access GCal
                       </button>
                     </div>
                   )}
@@ -686,61 +750,6 @@ function TimeSelectPage() {
                 ]}
                 isGeneralDays={isGeneralDays}
               />
-            </div>
-          </div>
-          <div className="flex flex-row justify-between mb-3">
-            {getAccountId() !== '' ? (
-              <div className="pl-5 z-60 mb-4 lg:mb-0">
-                <div className="hidden lg:flex">
-                  <ButtonSmall
-                    bgColor="primary"
-                    textColor="white"
-                    onClick={handleToggleGCalAvailabilitiesClick}
-                  >
-                    Show GCal Events
-                  </ButtonSmall>
-                </div>
-                <div className="lg:hidden flex">
-                  <ButtonSmall
-                    bgColor="primary"
-                    textColor="white"
-                    onClick={handleToggleGCalAvailabilitiesClick}
-                  >
-                    Show GCal Events
-                  </ButtonSmall>
-                </div>
-              </div>
-            ) : (
-              <div className="lg:pl-4 mb-4 lg:mb-0">
-                <button
-                  className="w-full lg:w-auto font-bold rounded-full shadow-md bg-white text-gray-600 py-3 px-4 text-sm lg:text-base
-                            flex items-center justify-center transform transition-transform hover:scale-95 active:scale-100"
-                  onClick={() => {
-                    signInWithGoogle(undefined, gapi, handleIsSignedIn).then(
-                      (loginSuccessful) => {
-                        if (loginSuccessful) {
-                          window.location.reload();
-                        } else {
-                          console.error('login failed');
-                        }
-                      }
-                    );
-                  }}
-                >
-                  <img src={LOGO} alt="Logo" className="mr-2 h-5 lg:h-6" />
-                  Sign in with Google to access GCal
-                </button>
-              </div>
-            )}
-            <div className="pr-5">
-              <ButtonSmall
-                bgColor="primary"
-                textColor="white"
-                themeGradient={false}
-                onClick={handleSubmitAvailability}
-              >
-                Next <span className="ml-1">&#8594;</span>
-              </ButtonSmall>
             </div>
           </div>
         </div>
--- src/components/utils/components/ButtonSmall.tsx
+++ src/components/utils/components/ButtonSmall.tsx
@@ -17,7 +17,7 @@ export default function ButtonSmall({
 }: Props) {
   return (
     <button
-      className={`text-sm md:text-md text-${textColor} transform transition-transform hover:scale-95 py-3 px-3 lg:py-3 lg:px-5 text-center rounded-full font-bold w-fit disabled:hover:scale-100 disabled:bg-gray-500 disabled:opacity-70 ${
+      className={`text-sm md:text-md text-${textColor} transform transition-transform hover:scale-95 py-3 px-3 lg:py-3 lg:px-5 text-center rounded-full font-bold w-fit disabled:hover:scale-100 disabled:bg-gray-500 disabled:opacity-70 whitespace-nowrap ${
         themeGradient
           ? `bg-gradient-to-r from-primary to-primary-dark dark:from-blue-900 dark:to-blue-600`
           : `bg-${bgColor}`
