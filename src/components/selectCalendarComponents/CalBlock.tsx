a3e3a314ea8fc6d1c78fded6a2807b1d82c67d9c
Hawa Khalif <hawa.khalif@yale.edu>
2025-04-17 18:53:52 -0400

    feat: add dark mode support for GCal events (#4a4d55)

--- src/components/selectCalendarComponents/CalBlock.tsx
+++ src/components/selectCalendarComponents/CalBlock.tsx
@@ -133,7 +133,13 @@ export default function CalBlock({
   }, [dragState, columnID, blockID, isAdmin, calendarState, user]);
 
   const getDefaultColor = useCallback(() => {
-    return isOnGcal ? '#dedfe3' : theme === 'light' ? 'white' : '#2d3748';
+    return isOnGcal 
+      ? theme === 'light' 
+        ? '#b3b4bd'  // Light mode color
+        : '#4a4d55'  // Dark mode color
+      : theme === 'light' 
+        ? 'white' 
+        : '#2d3748';
   }, [isOnGcal, theme]);
 
   const getGroupPercentageColor = useCallback(() => {
